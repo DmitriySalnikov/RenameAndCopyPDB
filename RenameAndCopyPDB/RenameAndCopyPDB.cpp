fd4f10077a832b0bb27ea426028f698270c831e7
DmitriySalnikov <salnikov.mine@yandex.ru>
2023-12-16 20:20:30 +0300

    fixed unicode conversions added cleanup added support for short relative names

--- RenameAndCopyPDB/RenameAndCopyPDB.cpp
+++ RenameAndCopyPDB/RenameAndCopyPDB.cpp
@@ -5,6 +5,9 @@
 #include <filesystem>
 #include <memory>
 #include <fstream>
+#include <vector>
+#include <algorithm>
+#include <system_error>
 
 // https://programmerall.com/article/19922097655/
 
@@ -13,20 +16,38 @@ enum Error {
 	FAIL = 1,
 	NO_PDB_DATA = 2,
 	DLL_LOCKED = 3,
-	DLL_TOO_BIG_ADDRESS = 4,
+	DLL_WRITE_FAIL = 4,
+	DLL_TOO_BIG_ADDRESS = 5,
 };
 
 struct PdbName {
 	size_t address;
-	std::string name;
+	std::string original_path;
+	std::wstring utf_name;
 };
 
-Error get_pdb_info(std::string dll_name, PdbName& pdb_info) {
-	HMODULE module = LoadLibraryA(dll_name.c_str());
+char* wc_to_utf8(const wchar_t* wc) {
+	int ulen = WideCharToMultiByte(CP_UTF8, 0, wc, -1, nullptr, 0, nullptr, nullptr);
+	char* ubuf = new char[ulen + 1];
+	WideCharToMultiByte(CP_UTF8, 0, wc, -1, ubuf, ulen, nullptr, nullptr);
+	ubuf[ulen] = 0;
+	return ubuf;
+}
+
+wchar_t* utf8_to_wc(const char* utf8) {
+	int wlen = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, nullptr, 0);
+	wchar_t* wbuf = new wchar_t[wlen + 1];
+	MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wbuf, wlen);
+	wbuf[wlen] = 0;
+	return wbuf;
+}
+
+Error get_pdb_info(std::wstring dll_name, PdbName& pdb_info) {
+	HMODULE module = LoadLibraryW(dll_name.c_str());
 
 	if (module == 0)
 	{
-		std::cout << "Failed load:" << dll_name << std::endl;
+		std::wcout << "Failed load:" << dll_name << std::endl;
 		return FAIL;
 	}
 
@@ -62,16 +83,8 @@ Error get_pdb_info(std::string dll_name, PdbName& pdb_info) {
 		PdbRSDS* pdb_rsds = (PdbRSDS*)(base_pointer + dbg_dir->AddressOfRawData);
 		if (0 == memcmp(&pdb_rsds->Signature, "RSDS", 4))
 		{
-			// WiNdOwS widechar to char...
-			int bufferSize = MultiByteToWideChar(CP_UTF8, 0, pdb_rsds->PdbFileName, -1, nullptr, 0);
-			std::wstring wideFilePath(bufferSize, 0);
-			MultiByteToWideChar(CP_UTF8, 0, pdb_rsds->PdbFileName, -1, &wideFilePath[0], bufferSize);
-
-			bufferSize = WideCharToMultiByte(CP_UTF8, 0, wideFilePath.c_str(), -1, nullptr, 0, nullptr, nullptr);
-			std::string utf8String(bufferSize, 0);
-			WideCharToMultiByte(CP_ACP, 0, wideFilePath.c_str(), -1, &utf8String[0], bufferSize, nullptr, nullptr);
-
-			pdb_info.name = utf8String;
+			pdb_info.original_path = pdb_rsds->PdbFileName;
+			pdb_info.utf_name = utf8_to_wc(pdb_rsds->PdbFileName);
 
 			int name_offset = (int)(reinterpret_cast<char*>(pdb_rsds->PdbFileName) - reinterpret_cast<char*>(pdb_rsds));
 			pdb_info.address = dbg_dir->PointerToRawData + name_offset;
@@ -82,7 +95,7 @@ Error get_pdb_info(std::string dll_name, PdbName& pdb_info) {
 	return OK;
 }
 
-Error patch_pdb_name(std::string dll, std::string pdb, size_t address, size_t original_size) {
+Error patch_pdb_name(std::wstring dll, std::wstring pdb, size_t address, size_t original_size) {
 	std::fstream file(dll, std::ios::in | std::ios::out | std::ios::binary);
 
 	if (!file.is_open()) {
@@ -94,14 +107,15 @@ Error patch_pdb_name(std::string dll, std::string pdb, size_t address, size_t or
 		return DLL_TOO_BIG_ADDRESS;
 	}
 
-	file.write(pdb.c_str(), pdb.size());
+	std::string updb = wc_to_utf8(pdb.c_str());
+	file.write(updb.c_str(), updb.size());
 	if (file.fail()) {
-		return DLL_LOCKED;
+		return DLL_WRITE_FAIL;
 	}
 
 	// Terminate string and fill original string with '\0'
 	// can be replaced by file.write("\0", 1);
-	int pad_size = (int)(original_size - pdb.length());
+	int pad_size = (int)(original_size - updb.length());
 	auto padding = std::unique_ptr<char>(new char[pad_size]);
 	memset(padding.get(), 0, pad_size);
 	file.write(padding.get(), pad_size);
@@ -110,26 +124,26 @@ Error patch_pdb_name(std::string dll, std::string pdb, size_t address, size_t or
 	return OK;
 }
 
-int main(int argc, char** argv) {
+int wmain(int argc, wchar_t** argv) {
 	std::setlocale(LC_ALL, "");
 
 	if (argc < 2) {
-		std::cout << "Please specify DLL file!" << std::endl;
+		std::wcout << "Please specify DLL file!" << std::endl;
 		return -1;
 	}
 
-	if (!std::filesystem::exists(argv[1])) {
-		std::cout << "DLL file does not exists!" << std::endl;
+	std::wstring dll_name = argv[1];
+	if (!std::filesystem::exists(dll_name)) {
+		std::wcout << "DLL file does not exists! " << dll_name << std::endl;
 		return -1;
 	}
 
-	std::string dll_name = argv[1];
 	// Rename to "[full/path/]~[dll_name]"
-	std::string new_dll_name = std::filesystem::path(dll_name).parent_path().append("~" + std::filesystem::path(dll_name).filename().string()).string();
+	std::wstring new_dll_name = std::filesystem::path(dll_name).parent_path().append(L"~" + std::filesystem::path(dll_name).filename().wstring()).wstring();
 
 	std::error_code err;
 	// Copy DLL like in Godot
-	std::filesystem::copy_file(argv[1], new_dll_name, std::filesystem::copy_options::overwrite_existing, err);
+	std::filesystem::copy_file(dll_name, new_dll_name, std::filesystem::copy_options::overwrite_existing, err);
 
 	if (err) {
 		std::cout << err.message() << std::endl;
@@ -141,14 +155,14 @@ int main(int argc, char** argv) {
 	Error err_code = get_pdb_info(new_dll_name, pdb_name);
 
 	if (err_code == OK) {
-		std::cout << "Original PDB name: " << pdb_name.name << std::endl;
+		std::wcout << "Original PDB name: " << pdb_name.utf_name << std::endl;
 	}
 	else if (err_code == NO_PDB_DATA) {
-		std::cout << "No PDB info found inside: " << new_dll_name << std::endl;
+		std::wcout << "No PDB info found inside: " << new_dll_name << std::endl;
 		return -1;
 	}
 	else {
-		std::cout << "Failed to rename PDB inside: " << dll_name << std::endl;
+		std::wcout << "Failed to rename PDB inside: " << dll_name << std::endl;
 		std::filesystem::remove(new_dll_name, err);
 		return -1;
 	}
@@ -156,33 +170,79 @@ int main(int argc, char** argv) {
 	// Save the new base name for the PDB. It will be relative, not absolute, as in Visual Studio by default.
 	// This is necessary so as not to change the size of the field with the PDB name, but simply replace it with a new name.
 	// TODO add logic to support short relative names
-	std::string new_pdb_base_name = std::filesystem::path(new_dll_name).filename().replace_extension("").string() + "_";
+	std::wstring new_pdb_base_name = L"~" + std::filesystem::path(pdb_name.utf_name).filename().replace_extension("").wstring() + L"_";
+
+	const size_t suffix_size = 3 /* 999 */ + 4 /* .pdb */;
+	size_t numbered_pdb_size = std::string(wc_to_utf8(new_pdb_base_name.c_str())).length() + suffix_size;
+	if (numbered_pdb_size > pdb_name.original_path.length()) {
+		if (pdb_name.original_path.length() < suffix_size + 1) {
+			std::cout << "The original PDB path length is too small: " << pdb_name.original_path << std::endl;
+			return -1;
+		}
+
+		new_pdb_base_name = std::wstring(utf8_to_wc(std::string(wc_to_utf8(new_pdb_base_name.c_str())).substr(0, pdb_name.original_path.length() - suffix_size).c_str()));
+	}
+
+	{
+		struct FilesDataWithTime {
+			std::filesystem::path path;
+			std::filesystem::file_time_type time;
+		};
+
+		// Clear old PDB files
+		std::vector<FilesDataWithTime> old_pdb_files;
+		for (int i = 0; i < 1000; i++) {
+			std::filesystem::path new_pdb_name = std::filesystem::path(dll_name).parent_path().append(new_pdb_base_name + std::to_wstring(i) + L".pdb");
+			if (std::filesystem::exists(new_pdb_name)) {
+				old_pdb_files.push_back({
+						new_pdb_name,
+						std::filesystem::last_write_time(new_pdb_name)
+					});
+			}
+		}
+
+		std::sort(old_pdb_files.begin(), old_pdb_files.end(), [](FilesDataWithTime first, FilesDataWithTime second) { return first.time < second.time; });
+		// Don't touch the newest one
+		if (old_pdb_files.size()) {
+			old_pdb_files.pop_back();
+		}
+
+		for (const auto& file : old_pdb_files) {
+			std::filesystem::remove(file.path, err);
+		}
+	}
 
 	for (int i = 0; i < 1000; i++) {
 		// Generate new name for PDB
-		std::string new_pdb_name = new_pdb_base_name + std::to_string(i) + ".pdb";
+		std::wstring new_pdb_name = new_pdb_base_name + std::to_wstring(i) + L".pdb";
+
 		// Try to copy PDB
 		std::filesystem::copy_file(
-			pdb_name.name,
-			std::filesystem::path(dll_name).parent_path().append(new_pdb_name).string(),
+			std::filesystem::path(pdb_name.utf_name).is_relative() ? std::filesystem::path(dll_name).parent_path().append(pdb_name.utf_name).wstring() : pdb_name.utf_name,
+			std::filesystem::path(dll_name).parent_path().append(new_pdb_name).wstring(),
 			std::filesystem::copy_options::overwrite_existing, err);
 
-		if (err) {
+		if (err == std::errc::no_such_file_or_directory) {
+			std::cout << err.message() << std::endl;
+			std::wcout << "PDB file not found: " << pdb_name.utf_name << std::endl;
+			return -1;
+		}
+		else if (err) {
 			std::cout << err.message() << std::endl;
 			continue;
 		}
 
-		err_code = patch_pdb_name(new_dll_name, new_pdb_name, pdb_name.address, pdb_name.name.length());
+		err_code = patch_pdb_name(new_dll_name, new_pdb_name, pdb_name.address, pdb_name.original_path.length());
 		if (err_code == OK) {
-			std::cout << "DLL patched. New PDB name: " << new_pdb_name << std::endl;
+			std::wcout << "DLL patched. New PDB name: " << new_pdb_name << std::endl;
 			break;
 		}
 		else if (err_code == DLL_LOCKED) {
-			std::cout << "DLL locked. Failed to patch DLL: " << new_dll_name << std::endl;
+			std::wcout << "DLL locked. Failed to patch DLL: " << new_dll_name << std::endl;
 			break;
 		}
 		else if (err_code == DLL_TOO_BIG_ADDRESS) {
-			std::cout << "PDB address was too big. Failed to patch DLL: " << new_dll_name << std::endl;
+			std::wcout << "PDB address was too big. Failed to patch DLL: " << new_dll_name << std::endl;
 			break;
 		}
 	}
